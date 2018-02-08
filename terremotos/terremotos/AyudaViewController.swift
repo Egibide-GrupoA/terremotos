25534557f2f47d56613e202cf2880c9c4d9aa331
Sheilapsm <murgia85@gmail.com>
2018-02-08 18:56:31 +0100

    interfaz ayuda

--- terremotos/terremotos/AyudaViewController.swift
+++ terremotos/terremotos/AyudaViewController.swift
@@ -28,17 +28,16 @@ class AyudaViewController: UIViewController {
         
         if sege.selectedSegmentIndex == 0{
             
-            texto.text = "1. Organiza y participa en simulacros de evacuación/n 2. Identifica las zonas de seguridad/n 3. Revisa las instalaciones de gas y luz/n 4.Almacena alimentos no perecederos y agua"
+        
         }
         
         if sege.selectedSegmentIndex == 1{
-            
-            texto.text = "1.Aléjate de ventanas y objetos que puedan caer/n 2.Converva la calma y ubicate en la zona de seguridad/n 3.Corta el suministro de gas y electricidad/n 4.Aléjate de postes, cables y marquesinas/n 5.Estacionate alejado de edificios altos"
+         
         }
         
         if sege.selectedSegmentIndex == 2{
             
-            texto.text = "1.Revisa las condiciones de tu casa/n 2.No enciendas cerillas o o velas hasta asegurarte de que no hay fugas de gas/N 3.Utiliza el teléfono sólo para emergencias/n 4.Mantente informatdo y atiende a las recomendaciones de las autoridades/n 5.Mantente alerta, se pueden presentar réplicas"
+           
             
         }
         
