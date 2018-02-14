5bc3d16378b23ee5bbe5fdaec355675f5504049d
Sheilapsm <murgia85@gmail.com>
2018-02-14 19:15:18 +0100

    ventana ajustes checkboxes

--- terremotos/terremotos/ViewController.swift
+++ terremotos/terremotos/ViewController.swift
@@ -12,6 +12,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+ 
+    
     @IBOutlet weak var lbl: UILabel!
     
     @IBAction func slider(_ sender: UISlider) {
@@ -19,19 +22,20 @@ class ViewController: UIViewController {
         lbl.text = String(sender.value)
     }
     
-    
-    
-    
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
