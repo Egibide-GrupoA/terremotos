ea47e1442a0856675cbd96326ad0f7beeb299ed5
Sheilapsm <murgia85@gmail.com>
2018-02-15 21:58:46 +0100

    lista table view

---
+++ terremotos/terremotos/ListaViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ListaViewController.swift
+//  terremotos
+//
+//  Created by  on 15/2/18.
+//  Copyright © 2018 grupoA. All rights reserved.
+//
+
+import UIKit
+
+class ListaViewController: UIViewController {
+
+    let list = [""]
+    
+    @IBOutlet weak var ListaTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
